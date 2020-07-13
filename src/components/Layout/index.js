f095398139bb7e2c955366926885403cf5ecc577
Elves Sousa <elves.sousa@gmail.com>
2020-07-13 12:09:30 -0300

    Lang atribute and meta description.

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,12 +3,17 @@ import { Helmet } from "react-helmet";
 
 import Header from "../Header";
 import Footer from "../Footer";
+import { useIntl } from "gatsby-plugin-intl";
 
 export default function Layout({ className, children, title }) {
+  const intl = useIntl();
   return (
     <main className={className}>
       <Helmet>
+        <html lang={intl.locale} />
+        <meta charSet="utf-8" />
         <title>{title}</title>
+        <meta name="description" content="A Gatsby multilingual site" />
       </Helmet>
       <Header />
       {children}
