e4f6d0291555e516456841c05ffddb15833d2e8b
Elves Sousa <elves.sousa@gmail.com>
2021-05-03 19:16:22 -0300

    Fix: About button now goes to the correct language page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,23 @@
 import React from "react";
-import { Link } from "gatsby";
+import {Link} from "gatsby";
 
 import Layout from "../components/Layout";
 
 import "../styles/layout.css";
-import { useIntl } from "gatsby-plugin-intl";
+import {useIntl} from "gatsby-plugin-intl";
 
 export default function Home() {
   const intl = useIntl();
+  const locale = intl.locale !== "pt" ? `/${intl.locale}` : "";
 
   return (
-    <Layout title={intl.formatMessage({ id: "home" })} className="home">
+    <Layout title={intl.formatMessage({id: "home"})} className="home">
       <section className="hero">
         <div className="message">
           <h1>Gatsby INTL</h1>
-          <p>{intl.formatMessage({ id: "slogan" })}</p>
-          <Link className="button" to="/about">
-            {intl.formatMessage({ id: "about" })}
+          <p>{intl.formatMessage({id: "slogan"})}</p>
+          <Link className="button" to={`${locale}/about`}>
+            {intl.formatMessage({id: "about"})}
           </Link>
         </div>
       </section>
